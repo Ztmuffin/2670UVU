0bb6d3635e643332f4a730dba236399cca78de01
Unknown <addisonbmoyer@gmail.com>
2017-08-25 14:03:50 -0600

    Class is done unity files

---
+++ Scripting2670/Assets/Scripts/MoveInputs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveInputs : MonoBehaviour {
+
+    Action InitialAction;
+	// Use this for initialization
+	void Start () {
+        InitialAction = Move;
+	}
+
+    private void Move()
+    {
+        print("Left Arrow");
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            InitialAction();
+       
+        }
+	}
+}
