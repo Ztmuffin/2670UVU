cd9e85d4e474d145e24b8414f2672dbb59fdd714
Unknown <addisonbmoyer@gmail.com>
2017-09-01 21:21:55 -0600

    So, practiced move and jump for Marty's assigment
    Jump is still Sporattic
    that is what i want to fix

---
+++ Scripting2670.1/Bear Prototypes/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CharacterMove : MonoBehaviour {
+
+	CharacterController charCon;
+	Vector2 movingMe;
+	public float speed = 3;
+	public float gravity = .6f;
+	public float jumpHeight = .5f;
+	private int numberOfJumps;
+
+
+	
+		void Start () {
+			charCon = GetComponent<CharacterController>();
+			MoveInput.JumpPress = Jump;
+			MoveInput.KeyPress += Move;
+			ResetDoubleJump();
+		}
+
+    private void Move(float _moving)
+    {
+		movingMe.y -= gravity*Time.deltaTime;
+		movingMe.x =_moving*speed*Time.deltaTime;
+		charCon.Move(movingMe);
+    }
+
+			private void Jump()
+			{
+				if (numberOfJumps > 0 || charCon.isGrounded)
+				{
+					movingMe.y += jumpHeight;
+					print("Jump");
+					numberOfJumps --;
+					ResetDoubleJump();
+				}
+			}
+					private void ResetDoubleJump()
+					{
+						if (charCon.isGrounded)
+						{
+							numberOfJumps = 1;
+						}
+					}
+    
+}
---
+++ Scripting2670.1/Bear Prototypes/Assets/Scripts/MoveInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class MoveInput : MonoBehaviour {
+	public static Action<float> KeyPress;
+	public static Action JumpPress; 
+	void FixedUpdate () {
+		if (KeyPress !=null)
+		{
+			KeyPress(Input.GetAxis("Horizontal"));
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			JumpPress();
+		}
+	}
+}
