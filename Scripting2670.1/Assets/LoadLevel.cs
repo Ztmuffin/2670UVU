14de665bd0026f2aa18041913adcd1fe45c239e7
Unknown <addisonbmoyer@gmail.com>
2017-09-08 14:03:27 -0600

    Leaving class for now
    did level script.. want to make a main menu script

---
+++ Scripting2670.1/Assets/LoadLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+public class LoadLevel : MonoBehaviour {
+
+	public static Action EndButtons;
+
+	void Start()
+	{
+		LoadLevel.EndButtons += EndThis;
+	}
+
+    private void EndThis()
+    {
+        GetComponent<Button>().interactable = false;
+    }
+
+    public string scener;
+	public void LoadLevels ()
+	{
+		SceneManager.LoadScene(scener, LoadSceneMode.Additive);
+		EndButtons();
+	}
+}
---
+++ Scripting2670.1/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void LoadLevels()
+	{
+
+	}
+}
+// Not done yet
