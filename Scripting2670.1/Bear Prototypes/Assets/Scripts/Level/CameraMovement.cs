c74ee7fd92bf94e3c41c4c660f4fb870c9f21a09
Unknown <addisonbmoyer@gmail.com>
2017-09-14 23:04:02 -0600

    The way it is stopps the camera move script when player passes through..
    may be a fun mechanic for edges of the map

--- Scripting2670.1/Bear Prototypes/Assets/Scripts/Level/CameraMovement.cs
+++ Scripting2670.1/Bear Prototypes/Assets/Scripts/Level/CameraMovement.cs
@@ -6,19 +6,43 @@ using System;
 
 public class CameraMovement : MonoBehaviour {
   public Transform player;
+  Camera camer;
   public Vector3 offset;
   bool cameraWillFollow = true;
+  // yes, i found this online, lets see if it does what i want it to do.  then i can adjust it.
+  // public Transform top;
+  //public Transform bottom;
+  public Transform left;
+  public Transform right;
+  
+  //private float maxY;
+  //private float minY;
+  private float maxX;
+  private float minX;
 
   void Start()
   {
-      StartCoroutine(CameraMoving());
+    camer = GetComponent<Camera>();
+    //maxY = top.transform.position.y;
+    //minY = bottom.transform.position.y;
+    maxX = right.transform.position.x;
+    minX = left.transform.position.x;
+
+
+    transform.position = new Vector3 (0, player.position.y + 3,  player.position.z - 20);
+    StartCoroutine(CameraMoving());
   }
   
   IEnumerator CameraMoving () 
   {
     while (cameraWillFollow)
     {
-      transform.position = new Vector3 (player.position.x + 1, player.position.y + 3,  player.position.z - 20); // Camera follows the player with specified offset position
+      // offset = camer.WorldToScreenPoint(player.position);
+      print("player is "+offset+" pixels from the left");
+      if (player.transform.position.x < maxX && player.transform.position.x > minX)
+      {
+        transform.position = new Vector3 (player.position.x, player.position.y + 3,  player.position.z - 20);
+      }
       yield return null;
     }
   }
