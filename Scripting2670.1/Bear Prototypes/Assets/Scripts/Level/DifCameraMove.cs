bf361cc27d0817a89a76447af291cfd3de11238c
Unknown <addisonbmoyer@gmail.com>
2017-09-17 17:36:27 -0600

    Camera Movement is working .  Yay

--- Scripting2670.1/Bear Prototypes/Assets/Scripts/Level/DifCameraMove.cs
+++ Scripting2670.1/Bear Prototypes/Assets/Scripts/Level/DifCameraMove.cs
@@ -27,17 +27,17 @@ Camera.position = new Vector3 (0, player.position.y,  player.position.z - 20);
     {
       if (player.transform.position.x < CameraPanRightTrigger.position.x && player.transform.position.x > CameraPanLeftTrigger.position.x)
 		{
-			Camera.position = new Vector3 (CameraPanLeftTrigger.position.x + 18, player.position.y + 3,  player.position.z - 20);
+			Camera.position = new Vector3 (Camera.position.x, player.position.y + 3,  player.position.z - 20);
 		}
       else if (player.transform.position.x > CameraPanRightTrigger.position.x)
       	{
-        Camera.position = new Vector3 (CameraPanRightTrigger.position.x + 18, player.position.y + 3,-20);
+        Camera.position = new Vector3 (Camera.position.x + 31, player.position.y + 3,-20);
         print("Max x is "+ CameraPanRightTrigger.position.x);
         print("minX is "+CameraPanLeftTrigger.position.x);
       	}
       else if (player.transform.position.x < CameraPanLeftTrigger.position.x)
       	{
-        Camera.position = new Vector3 (CameraPanLeftTrigger.position.x - 18, player.position.y + 3,-20);
+        Camera.position = new Vector3 (Camera.position.x - 31, player.position.y + 3,-20);
         print("Max x is "+ CameraPanRightTrigger.position.x);
         print("minX is "+CameraPanLeftTrigger.position.x);
       	}
