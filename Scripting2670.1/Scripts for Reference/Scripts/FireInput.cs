cd82706487d69ec928835119d72a687d9afe2af4
Unknown <addisonbmoyer@gmail.com>
2017-09-18 15:08:07 -0600

    adding trees

---
+++ Scripting2670.1/Scripts for Reference/Scripts/FireInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class FireInput : MonoBehaviour {
+
+	public static Action FireAction;
+	public static Action StopAction;
+	public void Fire () 
+		{
+			FireAction();
+		}
+
+	public void Stop()
+	{
+		StopAction();
+	}
+
+}
---
+++ Scripting2670.1/Scripts for Reference/Scripts/Weapons.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour {
+
+	public int ammoCount = 100;
+	public float fireRate = 1;
+	public float ammoPower = 0.1f;
+	public bool canFire = true;
+	void Awake()
+	{
+		FireInput.FireAction += FireHandler;
+		FireInput.StopAction += StopHandler;
+	}
+
+
+    void FireHandler () {
+		StartCoroutine(Fire());
+		print("Starting Corutines");
+	}
+	void StopHandler()
+    {
+		StopAllCoroutines();
+		print("Stoping All Corutines");
+    }
+
+	
+	// Update is called once per frame
+	public IEnumerator Fire () 
+	{
+		while (canFire)
+		{
+			if (ammoCount >0)
+			{
+				ammoCount--;
+				print(ammoCount);
+				yield return new WaitForSeconds (1);
+			}
+			else if (ammoCount < 1)
+			{
+				canFire = false;
+				print("you're Out Of Ammo");
+			}
+			
+		}
+		
+	}
+}
