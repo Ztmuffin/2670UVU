b218870a88bd3662ba12cbfc31cc003005c3e99b
Unknown <addisonbmoyer@gmail.com>
2017-10-08 23:10:49 -0600

    Added Torch Like Checkpoints

---
+++ Scripting2670.1/Get Home Game/Get Home/Assets/Scripts/Level/Checkpoints.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour {
+
+	public Transform startPosition;
+	public Transform placeCheckpointHere;
+
+	void OnTriggerEnter(Collider other)
+	{
+		startPosition.position = placeCheckpointHere.position;
+	}
+}
