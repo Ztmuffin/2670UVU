a11cd21f94f4a794c5e6b38024d200ca2bc6a676
Unknown <addisonbmoyer@gmail.com>
2017-10-09 16:28:08 -0600

    Before Deleting Swamp scene after merging with temple scene
    Build doesn't have enough "memory"

---
+++ Scripting2670.1/Bear Prototypes/Assets/Scripts/TuturialsFromClass/SendHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SendHealth {
+
+public static Action<float> HealthAction; // Accessable from everywhere
+public static float health = 1.0f;  // Accessable from everywhere
+
+public static void UpdateHealth(float _power)  // Accessable from everywhere
+{
+	health += _power;
+	if (health < 0)
+		health = 0;
+	if (health >1)
+		health = 1; 
+
+	HealthAction(health);
+}
+}
