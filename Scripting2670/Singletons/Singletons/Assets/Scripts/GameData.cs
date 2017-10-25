fbe5595cca31baaa7903b127f0f9a1b4a91c45dd
Unknown <addisonbmoyer@gmail.com>
2017-10-25 15:46:54 -0600

    before saving changes to vine 2

---
+++ Scripting2670/Singletons/Singletons/Assets/Scripts/GameData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class GameData{
+
+//	public float health = 1;
+
+	protected GameData()
+	{
+
+	}
+
+	protected string dataName = "GameData";
+	private static GameData instance;
+
+	public static GameData Instance
+	{
+		get {
+				if (instance == null)
+				{
+					GetData();
+				}
+				return instance;
+			}
+	}
+
+	public static void GetData ()
+	{
+		if (string.IsNullOrEmpty(PlayerPrefs.GetString(dataName)))
+		{
+			instance = new GameData();
+		}
+		else
+		{
+			instance = JsonUtility.FromJson<GameData>(PlayerPrefs.GetString(dataName));
+		}
+		
+	}
+	public static void SetData ()
+	{
+		PlayerPrefs.SetString(dataName, JsonUtility.ToJson(instance));
+	}
+}
---
+++ Scripting2670/Singletons/Singletons/Assets/Scripts/PurchaseData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseData : GameData {
+
+	public float health;
+
+	PurchaseData()
+	{
+	dataName = "PurchaseData";
+	}
+}
