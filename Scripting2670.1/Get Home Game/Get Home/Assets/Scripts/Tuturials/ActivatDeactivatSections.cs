702e044e60e270fca79b5d654a06ec904e7e2eb7
Unknown <addisonbmoyer@gmail.com>
2017-10-25 17:54:40 -0600

    leaving to home

---
+++ Scripting2670.1/Get Home Game/Get Home/Assets/Scripts/Tuturials/ActivatDeactivatSections.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatDeactivatSections : MonoBehaviour {
+
+	public GameObject ManageThis;
+	void OnTriggerStay(Collider other)
+	{
+		ManageThis.SetActive(true);
+	}
+	void OnTriggerExit(Collider other)
+	{
+		ManageThis.SetActive(false);
+	}
+}
